8d4ebb89186a6cc0eb14c4bc05a59f31b681514f
HanRui Liu <han2002@uw.edu>
2025-03-07 12:29:38 -0800

    added some heatmap stuff

--- script.js
+++ script.js
@@ -32,6 +32,64 @@ map.on('load', () => {
         data: data
       });
 
+      // Add heatmap layer
+      map.addLayer({
+        id: 'transitRoutesHeatmap',
+        type: 'heatmap',
+        source: 'transitRoutes',
+        maxzoom: 15,
+        paint: {
+          // Increase the heatmap weight based on frequency and property magnitude
+          'heatmap-weight': [
+            'interpolate',
+            ['linear'],
+            ['get', 'magnitude'],
+            0, 0,
+            6, 1
+          ],
+          // Increase the heatmap color weight weight by zoom level
+          // heatmap-intensity is a multiplier on top of heatmap-weight
+          'heatmap-intensity': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            0, 1,
+            15, 3
+          ],
+          // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+          // Begin color ramp at 0-stop with a 0-transparancy color
+          // to create a blur-like effect.
+          'heatmap-color': [
+            'interpolate',
+            ['linear'],
+            ['heatmap-density'],
+            0, 'rgba(33,102,172,0)',
+            0.2, 'rgb(103,169,207)',
+            0.4, 'rgb(209,229,240)',
+            0.6, 'rgb(253,219,199)',
+            0.8, 'rgb(239,138,98)',
+            1, 'rgb(178,24,43)'
+          ],
+          // Adjust the heatmap radius by zoom level
+          'heatmap-radius': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            0, 2,
+            15, 20
+          ],
+          // Transition from heatmap to circle layer by zoom level
+          'heatmap-opacity': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            7, 1,
+            15, 0
+          ],
+        }
+      });
+
+      // Add line layer for routes
       map.addLayer({
         id: 'transitRoutesLayer',
         type: 'line',
@@ -219,6 +277,17 @@ map.on('load', () => {
             { hover: false }
           );
           highlightedRouteId = null;
+        }
+      });
+
+      // Toggle heatmap visibility
+      const toggleHeatmapButton = document.getElementById('toggleHeatmap');
+      toggleHeatmapButton.addEventListener('click', () => {
+        const visibility = map.getLayoutProperty('transitRoutesHeatmap', 'visibility');
+        if (visibility === 'visible') {
+          map.setLayoutProperty('transitRoutesHeatmap', 'visibility', 'none');
+        } else {
+          map.setLayoutProperty('transitRoutesHeatmap', 'visibility', 'visible');
         }
       });
     })
